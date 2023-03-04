de229274dfddcffac649fe7908da1fee0f4d597a
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-03-04 15:31:18 +0900

    Add files via upload

---
+++ Python_practiceI_Booleans-and-Conditionals_else.py
@@ -0,0 +1,8 @@
+money = 100
+apple_price = 200
+
+if money >= apple_price:
+    print('You can buy apples.')
+# Please output "You don't have enough money." if the condition of the if statement is not met.
+else:
+  print("You don't have enough money.")
